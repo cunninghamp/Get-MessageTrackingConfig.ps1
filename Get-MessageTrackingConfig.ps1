0375f08cf3f708ba70286c745b4aaadb2f081ab0
CalicoPanda <bruce.hinkofer@gmail.com>
2016-09-15 08:49:58 -0400

    Update Get-MessageTrackingConfig.ps1
    added html report with email options

--- Get-MessageTrackingConfig.ps1
+++ Get-MessageTrackingConfig.ps1
@@ -1,4 +1,4 @@
-﻿<#
+<#
 .SYNOPSIS
 Get-MessageTrackingConfig.ps1
 
@@ -7,6 +7,10 @@ Generate a CSV report of the message tracking configuration of your Exchange 201
 
 .EXAMPLE
 .\Get-MessageTrackingConfig.ps1
+
+.EXAMPLE
+.\Get-MessageTrackingConfig.ps1 -SendEmail -MailFrom:exchangeserver@exchangeserverpro.net -MailTo:paul@exchangeserverpro.com -MailServer:smtp.exchangeserverpro.net
+Sends an email report with CSV file attached.
 
 .LINK
 
@@ -28,29 +32,59 @@ Find me on:
 
 Change Log:
 V1.00, 14/04/2015 - Initial version
+V1.01, 09/15/2016 - Added html report with email options
 #>
 
 #requires -version 2
 
 [CmdletBinding()]
 
-param ()
+param (
+
+	[Parameter( Mandatory=$false)]
+	[switch]$SendEmail,
+
+	[Parameter( Mandatory=$false)]
+	[string]$MailFrom,
+
+	[Parameter( Mandatory=$false)]
+	[string]$MailTo,
+
+	[Parameter( Mandatory=$false)]
+	[string]$MailServer
+
+)
 
 #----------------------------------------
 # Initialize
 #----------------------------------------
 
-if (Test-Path $env:ExchangeInstallPath\bin\RemoteExchange.ps1)
+#Add Exchange 2010/2013 snapin if not already loaded in the PowerShell session
+if (!(Get-PSSnapin | where {$_.Name -eq "Microsoft.Exchange.Management.PowerShell.E2010"}))
 {
-    . $env:ExchangeInstallPath\bin\RemoteExchange.ps1
-	Connect-ExchangeServer -auto -AllowClobber
-}
-else
-{
-    Write-Host "Exchange management tools are not installed on this computer."
-    EXIT
-}
+	try
+	{
+		Add-PSSnapin Microsoft.Exchange.Management.PowerShell.E2010 -ErrorAction STOP
+	}
+	catch
+	{
+		#Snapin was not loaded
+		Write-Warning $_.Exception.Message
+		EXIT
+	}
 
+    if (Test-Path $env:ExchangeInstallPath\bin\RemoteExchange.ps1)
+    {
+        . $env:ExchangeInstallPath\bin\RemoteExchange.ps1
+	    Connect-ExchangeServer -auto -AllowClobber
+    }
+    else
+    {
+        Write-Host "Exchange management tools are not installed on this computer."
+        EXIT
+    }
+
+} # endif check Exchange Snapin
 
 #----------------------------------------
 # Variables
@@ -60,11 +94,28 @@ $Exchange2013Report = @()
 $Exchange2010Report = @() # Exchange 2010 servers not supported by this script yet
 
 $now = Get-Date
+$date = $now.ToShortDateString()
 
+$reportemailsubject = "Exchange Message Tracking Configuration Report - $date"
+$myDir              = Split-Path -Parent $MyInvocation.MyCommand.Path
+$reportfile = "$myDir\MessageTrackingConfig2013.csv"
+
+#...................................
+# Email Settings
+#...................................
+
+$smtpsettings = @{
+	To =  $MailTo
+	From = $MailFrom
+    Subject = $reportemailsubject
+	SmtpServer = $MailServer
+	}
 
 #----------------------------------------
 # Main Script
 #----------------------------------------
+
+Write-Host "Fetching list of Servers"
 
 $exchangeservers = @(Get-ExchangeServer | Where {$_.IsHubTransportServer -or $_.IsMailboxServer} | Sort AdminDisplayVersion,Name)
 
@@ -105,7 +156,7 @@ foreach ($srv in $exchangeservers)
         {
             Write-Warning $_.Exception.Message
         }
-    }
+    } # endif Version 15
 
     if ($srv.AdminDisplayVersion -like "Version 14*")
     {
@@ -131,7 +182,6 @@ foreach ($srv in $exchangeservers)
             Write-Warning $_.Exception.Message
         }
     }
-
    
     # Process Exchange 2013 Mailbox Server
     if ($version -eq "Exchange Server 2013")
@@ -200,18 +250,19 @@ foreach ($srv in $exchangeservers)
         }
         else
         {
-            # Message tracking is not enabled for this server
-        }
+            Write-Host -ForegroundColor White "Message tracking is not enabled for this server"
+        } # endif Message tracking is not enabled for this server
 
     $Exchange2013Report += $serverObj
     
-    }
+    } # endif process 2013 server
 
     # Process Exchange 2010 Server
     if ($version -eq "Exchange Server 2010")
     {
         # Still to come
-    }
+    } # endif process 2010 server
+
 }
 
 Write-Verbose "------ All servers completed"
@@ -220,15 +271,45 @@ Write-Verbose "------ All servers completed"
 # Output the reports to CSV
 if ($Exchange2013Report)
 {
-     $Exchange2013Report | Export-Csv MessageTrackingConfig2013.csv -NoTypeInformation
-}
+    Write-Host -ForegroundColor White "Saving report to $reportfile"
+	$Exchange2013Report | Export-Csv $reportfile -NoTypeInformation -Encoding UTF8
+} # endif 2013 report
 
 if ($Exchange2010Report)
 {
-    #  $Exchange2010Report | Export-Csv MessageTrackingConfig2010.csv -NoTypeInformation  # 2010 Version Not supported
-}
+    Write-Host -ForegroundColor White "Saving report to $reportfile"
+	$Exchange2010Report | Export-Csv $reportfile -NoTypeInformation -Encoding UTF8
+} # endif 2010 report
 
+if ($SendEmail)
+{
+
+    $reporthtml = $Exchange2013Report | ConvertTo-Html -Fragment
+
+	$htmlhead="<html>
+				<style>
+				BODY{font-family: Arial; font-size: 8pt;}
+				H1{font-size: 22px; font-family: 'Segoe UI Light','Segoe UI','Lucida Grande',Verdana,Arial,Helvetica,sans-serif;}
+				H2{font-size: 18px; font-family: 'Segoe UI Light','Segoe UI','Lucida Grande',Verdana,Arial,Helvetica,sans-serif;}
+				H3{font-size: 16px; font-family: 'Segoe UI Light','Segoe UI','Lucida Grande',Verdana,Arial,Helvetica,sans-serif;}
+				TABLE{border: 1px solid black; border-collapse: collapse; font-size: 8pt;}
+				TH{border: 1px solid #969595; background: #dddddd; padding: 5px; color: #000000;}
+				TD{border: 1px solid #969595; padding: 5px; }
+				td.pass{background: #B7EB83;}
+				td.warn{background: #FFF275;}
+				td.fail{background: #FF2626; color: #ffffff;}
+				td.info{background: #85D4FF;}
+				</style>
+				<body>
+                <p>Report of Exchange Message Tracking Configuration as of $date. CSV version of report attached to this email.</p>"
+		
+	$htmltail = "</body></html>"	
+
+	$htmlreport = $htmlhead + $reporthtml + $htmltail
+
+	Send-MailMessage @smtpsettings -Body $htmlreport -BodyAsHtml -Encoding ([System.Text.Encoding]::UTF8) -Attachments $reportfile
+} # endif send email
 
 #----------------------------------------
-# Finished
+# Finished!
 #----------------------------------------
